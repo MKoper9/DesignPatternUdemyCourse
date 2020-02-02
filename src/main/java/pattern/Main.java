9719355f21664a32c3d66090f27ca9eb6a2e88da
Michal Koperski <mkoperpno@gmali.com>
2020-02-02 19:31:05 +0100

    Create class UKToContinentalAdapter and implements in Main

--- src/main/java/pattern/Main.java
+++ src/main/java/pattern/Main.java
@@ -1,9 +1,6 @@
 package pattern;
 
-import pattern.patternsAdapter.ContinentalDevice;
-import pattern.patternsAdapter.ContinentalSocket;
-import pattern.patternsAdapter.UKDevice;
-import pattern.patternsAdapter.UKSocket;
+import pattern.patternsAdapter.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,6 +23,8 @@ public class Main {
         UKSocket ukSocket = new UKSocket();
         ukSocket.plugIn(ukRadio);
 
-        continentalSocket.plugIn(ukRadio);
+        UKToContinentalAdapter adapter = new UKToContinentalAdapter(ukRadio);
+
+        continentalSocket.plugIn(adapter);
     }
 }
---
+++ src/main/java/pattern/patternsAdapter/UKToContinentalAdapter.java
@@ -0,0 +1,15 @@
+package pattern.patternsAdapter;
+
+public class UKToContinentalAdapter implements ContinentalDevice {
+
+    UKDevice device;
+
+    public UKToContinentalAdapter(UKDevice device) {
+        this.device = device;
+    }
+
+    @Override
+    public void on() {
+        device.powerOn();
+    }
+}
