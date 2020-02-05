bb60870ed390052daeb17f06590d24120bfb69b1
Michal Koperski <mkoperpno@gmali.com>
2020-02-05 22:48:56 +0100

    Memento: done memento example

---
+++ src/main/java/memento/smart_app/SmartAppMemento.java
@@ -0,0 +1,15 @@
+package memento.smart_app;
+
+class SmartAppMemento {
+
+    private Double version;
+
+    SmartAppMemento(Double version) {
+        this.version = version;
+    }
+
+    Double getVersion() {
+        return version;
+    }
+
+}
