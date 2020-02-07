80540152bc42880a47b8a2a3bf4712f5633dc1b0
Michal Koperski <mkoperpno@gmali.com>
2020-02-07 21:11:45 +0100

    Fasade: make pattern problem

---
+++ src/main/java/fasade/Main.java
@@ -0,0 +1,23 @@
+package fasade;
+
+import fasade.deliverybox.DeliveryBox;
+import fasade.deliverybox.DeliveryBoxSystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        pickupPackage();
+
+    }
+
+    private static void pickupPackage() {
+        DeliveryBox deliveryBox = new DeliveryBox();
+        DeliveryBoxSystem deliveryBoxSystem = new DeliveryBoxSystem();
+
+        deliveryBox.getUserData();
+        if (deliveryBoxSystem.isUserDataValidated() && deliveryBoxSystem.isPaymentSecured()) {
+            deliveryBox.openBox();
+        }
+    }
+}
---
+++ src/main/java/fasade/deliverybox/DeliveryBox.java
@@ -0,0 +1,23 @@
+package fasade.deliverybox;
+
+public class DeliveryBox {
+    public boolean isDeliveryFull(){
+        System.out.println("Delivery Box is not full");
+        return false;
+    }
+
+    public boolean isDeliveryBoxBroken(){
+        System.out.println("Delivery Box in not broken");
+        return false;
+    }
+
+    public void getUserData(){
+        System.out.println("User data entered.");
+    }
+
+    public void openBox(){
+        System.out.println("Box opened.");
+    }
+
+
+}
